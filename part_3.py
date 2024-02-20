6b87f659ea637f7bf28d7787d168f554a4379128
Krish Patel <2021kpatel@gmail.com>
2024-02-20 09:50:36 -0500

    worked on part 3 - still some errors

--- part_3.py
+++ part_3.py
@@ -11,24 +11,30 @@ sr= 44100
 #function to create the chirp signal
 def generate_chirp_tone(duration):
     # Time array for the sine tone
-    time = np.arange(0, duration, 1/sr)
+    time = np.arange(0, duration, int(sr*duration))
     # Frequency of the sine tone varying 0 to 8000Hz
-    frequency_range = np.linspace(0, 8000, sr*duration)
+    frequency = np.linspace(0, 8000, len(time))
     # Generate the sine tone
-    sine_wave = np.sin(2 * np.pi * frequency * time)
+    chirp_wave = np.sin(2 * np.pi * frequency * time)
     # Play the sine tone
-    sd.play(sine_wave, samplerate=sr)
+    sd.play(chirp_wave, samplerate=sr)
     sd.wait()  # Wait until the sine tone is done playing
     # Save the sine tone to a WAV file
-    write(f'teamKKA-sinetone.wav', sr, sine_wave)
+    write(f'teamKKA-chirp.wav', sr, chirp_wave)
 
     # Plot the spectrogram of the sine tone
     plt.figure(figsize=(10, 4))
-    librosa.display.specshow(librosa.amplitude_to_db(np.abs(librosa.stft(sine_wave)), ref=np.max), sr=sr, x_axis='time', y_axis='log')
+    librosa.display.specshow(librosa.amplitude_to_db(np.abs(librosa.stft(chirp_wave)), ref=np.max), sr=sr, x_axis='time', y_axis='log')
     plt.colorbar(format='%+2.0f dB')
-    plt.title('Spectrogram of Sine Tone')
+    plt.title('Spectrogram of Chirp Signal')
     plt.xlabel('Time (s)')
     plt.ylabel('Frequency (Hz)')
     plt.tight_layout()
-    plt.savefig('sine_tone_spectrogram.png')  # Save spectrogram as PNG image
+    plt.savefig('chirp_spectrogram.png')  # Save spectrogram as PNG image
     plt.show()
+    
+# Ask for speech duration input (I'm setting this to 7 seconds by default)
+speech_duration = float(input("Enter the duration of the speech file in seconds: "))
+
+# Generate sine tone with the provided duration
+generate_chirp_tone(speech_duration)
