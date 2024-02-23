8ac106ff1d421508c4b814ae8f308b38ae6ea90e
Kyle Rabbitt <kerabbitt@wpi.edu>
2024-02-23 14:02:20 -0500

    combined audio part 5

--- part5.py
+++ part5.py
@@ -1,12 +1,15 @@
 import librosa
 import IPython as ip
 from scipy.io.wavfile import write
+import numpy as np
+import sounddevice as sd
 
-#samplind_rate1 = sampling_rate2 = 44100
 y1, sample_rate1 = librosa.load('quick_brown_fox_MONO.wav', mono=True)
 y2, sample_rate2 = librosa.load('teamKKA-sinetone.wav', mono=True)
+sr=44100
 
-print(librosa.util.stack([y1, y2], axis=1))
+output = y1+(y2*0.5)
 
-
-#write(f"teamKKA-speechchirp.wav", sr, )
+sd.play(output, samplerate=sr)
+sd.wait()
+write(f"teamKKA-speechchirp.wav", sr, output)
