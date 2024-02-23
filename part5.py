032b88a928706c68184a295a09b654e953e0ae4c
Kyle Rabbitt <kerabbitt@wpi.edu>
2024-02-23 14:05:41 -0500

    fixed input read sample rate

--- part5.py
+++ part5.py
@@ -4,8 +4,8 @@ from scipy.io.wavfile import write
 import numpy as np
 import sounddevice as sd
 
-y1, sample_rate1 = librosa.load('quick_brown_fox_MONO.wav', mono=True)
-y2, sample_rate2 = librosa.load('teamKKA-sinetone.wav', mono=True)
+y1, sample_rate1 = librosa.load('quick_brown_fox_MONO.wav',sr=44100, mono=True)
+y2, sample_rate2 = librosa.load('teamKKA-sinetone.wav', sr=44100,mono=True)
 sr=44100
 
 output = y1+(y2*0.5)
